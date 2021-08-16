f4a33da4d0c3d39b4e1cc6e8eb0b2c92fddcbd04
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-17 03:32:59 +0530

    feat: Find frequency of words from sentence

--- HashTableDemo/Program.cs
+++ HashTableDemo/Program.cs
@@ -8,6 +8,25 @@ namespace HashTableDemo
         {
             //welcome Message
             Console.WriteLine("Welcome to HASH MAP DEMO!");
+            
+            //Object of class MyMapNode
+            MyMapNode<string, string> myMapNode = new MyMapNode<string, string>(5);
+
+            //Add data
+            myMapNode.Add("0", "To");
+            myMapNode.Add("1", "be");
+            myMapNode.Add("2", "To");
+            myMapNode.Add("3", "or");
+            myMapNode.Add("4", "not");
+            myMapNode.Add("5", "be");
+
+            //Get Value and Display
+            string hash5 = myMapNode.Get("5");
+            Console.WriteLine("5th index  value: " + hash5);
+
+            //Get Value and Display
+            string hash2 = myMapNode.Get("2");
+            Console.WriteLine("2nd index  value: " + hash2);
         }
     }
 }
