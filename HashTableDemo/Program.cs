0f720fdaecb681367673a6217d140ec5ad0f6459
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-17 03:20:47 +0530

    feat: Welcome Message

---
+++ HashTableDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HashTableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //welcome Message
+            Console.WriteLine("Welcome to HASH MAP DEMO!");
+        }
+    }
+}
